951cbc3b5faddd83c5529ddd4a202404fd418570
minghaozhi <597575122@qq.com>
2019-03-22 18:55:15 +0800

    统一授权认证中心

--- mycloud-oauth2/src/main/java/com/moshang/oauth2/config/AuthorizationServerConfig.java
+++ mycloud-oauth2/src/main/java/com/moshang/oauth2/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.moshang.oauth2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -28,6 +29,7 @@ import javax.sql.DataSource;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
+    @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
     @Autowired
     private DataSource dataSource;
@@ -47,14 +49,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 //        return new JdbcTokenStore(dataSource);
 //    }
 
+    /**
+     *
+     * 这个方法主要是用于校验注册的第三方客户端的信息，可以存储在数据库中，默认方式是存储在内存中，如下所示，注释掉的代码即为内存中存储的方式
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.withClientDetails(clientDetails());
     }
-    @Bean
+    @Bean // 声明 ClientDetails实现
     public ClientDetailsService clientDetails() {
         return new JdbcClientDetailsService(dataSource);
     }
+    /**
+     * 这个方法主要的作用用于控制token的端点等信息
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(redisTokenStore())
@@ -78,11 +87,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         tokenServices.setRefreshTokenValiditySeconds(60 * 60 * 24 * 7);//默认30天，这里修改
         return tokenServices;
     }
-
+    /**
+     允许表单验证，浏览器直接发送post请求即可获取tocken
+     */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()");
         security .checkTokenAccess("isAuthenticated()");
         security.allowFormAuthenticationForClients();
     }
+
 }
---
+++ mycloud-oauth2/src/main/java/com/moshang/oauth2/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.moshang.oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @program: MyCloud
+ * @description: ${description}
+ * @author: moshang
+ * @create: 2019-03-22 18:37
+ **/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private MyAuthenticationProvider provider;
+
+    @Autowired
+    private UserDetailsService userService;
+    /**
+     * 用户认证
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       // auth.authenticationProvider(provider);
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+//      auth.inMemoryAuthentication()
+//            .withUser("user").password("123456").authorities("ROLE_USER");
+    }
+
+    /**
+     * 1:
+     * 请求授权:
+     * spring security 使用以下匹配器来匹配请求路劲：
+     *      antMatchers:使用ant风格的路劲匹配
+     *      regexMatchers:使用正则表达式匹配路劲
+     * anyRequest:匹配所有请求路劲
+     * 在匹配了请求路劲后，需要针对当前用户的信息对请求路劲进行安全处理。
+     * 2:定制登录行为。
+     *      formLogin()方法定制登录操作
+     *      loginPage()方法定制登录页面访问地址
+     *      defaultSuccessUrl()登录成功后转向的页面
+     *      permitAll()
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    /**
+     * 密码模式下必须注入的bean authenticationManagerBean
+     * 认证是由 AuthenticationManager 来管理的，
+     * 但是真正进行认证的是 AuthenticationManager 中定义的AuthenticationProvider。
+     *  AuthenticationManager 中可以定义有多个 AuthenticationProvider
+     */
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}

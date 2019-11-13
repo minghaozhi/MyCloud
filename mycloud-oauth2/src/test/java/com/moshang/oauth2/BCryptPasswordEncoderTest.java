b909ccba680a35dc6a276e53c56235bf17132385
xieweiwei <xieweiwei@mainbo.com>
2019-11-13 19:05:21 +0800

    加密解密测试方法

--- mycloud-oauth2/src/test/java/com/moshang/oauth2/BCryptPasswordEncoderTest.java
+++ mycloud-oauth2/src/test/java/com/moshang/oauth2/BCryptPasswordEncoderTest.java
@@ -9,12 +9,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 public class BCryptPasswordEncoderTest {
 
-
+    /**
+     * 加密
+     */
     @Test
     public  void BCryptPassword(){
         String password="123456";
         BCryptPasswordEncoder encoder =new BCryptPasswordEncoder();
         System.out.println(encoder.encode(password));
+
+
+    }
+
+    /**
+     * 校验
+     */
+    @Test
+    public  void verify(){
+        String password="123456";
+        BCryptPasswordEncoder encoder =new BCryptPasswordEncoder();
+        boolean flag = encoder.matches(password, "$2a$10$M17TE5layM8oBu6xyKBfIeIk8iUG5Bz6gpdN50jswyVrZxTvtqTLW");
+        System.out.println(flag);
 
 
     }
